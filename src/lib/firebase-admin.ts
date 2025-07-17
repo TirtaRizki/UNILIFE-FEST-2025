f9d6ca74b53b24aaa19b59e95199a2548dd1172d
Tirta Rizki Ramadhann <tirtarzk30@gmail.com>
2025-07-17 12:18:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -5,11 +5,11 @@ import { getApps } from 'firebase-admin/app';
 // only be available on the server.
 
 const serviceAccount = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
   // The private key must be correctly formatted to be parsed from the environment variable.
   // When setting FIREBASE_PRIVATE_KEY in your .env.local, wrap the key in quotes.
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
 if (!getApps().length) {
